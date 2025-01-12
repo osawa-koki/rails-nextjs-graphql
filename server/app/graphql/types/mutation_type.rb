dbe679ea848b0bdbefa186ccd1235f16cb1e5f2f
osawa-koki <koki@osawa.dev>
2025-01-12 22:25:19 +0900

    Railsの初期化

---
+++ server/app/graphql/types/mutation_type.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Types
+  class MutationType < Types::BaseObject
+    field :create_item, mutation: Mutations::CreateItem
+    field :update_item, mutation: Mutations::UpdateItem
+    field :delete_item, mutation: Mutations::DeleteItem
+  end
+end
